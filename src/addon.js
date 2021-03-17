2d2d60db4f7d9559cd19cb4411468ef1749c597d
coffee-monk <coffeemonk@hotmail.com>
2021-03-17 14:47:05 -0400

    minimizing

--- src/addon.js
+++ src/addon.js
@@ -1,16 +1,24 @@
-import Channel from 'js-channel';
-import EventEmitter from 'eventemitter3';
-import { Promise } from 'es6-promise';
-import { iframeResizerContentWindow } from 'iframe-resizer';
-import * as _ from 'lodash';
-import API from './api';
+import Channel from "js-channel";
+import EventEmitter from "eventemitter3";
+import { Promise } from "es6-promise";
+import * as _ from "lodash";
+import API from "./api";
 
-import iframeResizerOptions from './iframe-resizer-options';
+window.iFrameResizer = {
+  heightCalculationMethod() {
+    const body = document.body;
+    const html = document.documentElement;
 
-window.iFrameResizer = iframeResizerOptions;
+    return Math.max.apply(null, [
+      body.scrollHeight,
+      body.offsetHeight,
+      html.offsetHeight,
+    ]);
+  },
+};
 
 class Addon extends EventEmitter {
-  constructor (options={}) {
+  constructor(options = {}) {
     super();
 
     this.options = options;
@@ -18,119 +26,144 @@ class Addon extends EventEmitter {
 
     this.channel = Channel.build({
       window: options.window || window.parent,
-      origin: '*',
+      origin: "*",
       scope: options.id || location.origin,
       postMessageObserver: (origin, message) => {
-        this.emit('postMessage', origin, message);
+        this.emit("postMessage", origin, message);
       },
       gotMessageObserver: (origin, message) => {
-        this.emit('gotMessage', origin, message);
+        this.emit("gotMessage", origin, message);
       },
     });
 
-    for (let event of ['init', 'update', 'reload', '_event']) {
+    for (let event of ["init", "update", "reload", "_event"]) {
       this.channel.bind(event, (tx, data) => {
-        let eventName = event, eventData = data;
+        let eventName = event,
+          eventData = data;
 
-        if (event === '_event') {
+        if (event === "_event") {
           eventName = data.eventName;
           eventData = data.eventData;
         }
         this.emit(eventName, eventData);
 
-        return 'success';
-      })
+        return "success";
+      });
     }
   }
 
-  request (params) {
+  request(params) {
     return new Promise((resolve, reject) => {
-      if (!_.isPlainObject(params)) throw new Error('Params must be an object');
+      if (!_.isPlainObject(params)) throw new Error("Params must be an object");
 
       let method, endpoint, query, body;
       ({ method, endpoint, query, body } = params);
 
       if (!method || !endpoint || !_.isString(method) || !_.isString(endpoint))
-        throw new Error('Invalid method or endpoint');
+        throw new Error("Invalid method or endpoint");
 
       if (!_.isUndefined(query) && !_.isPlainObject(query))
-        throw new Error('Query must be an object');
+        throw new Error("Query must be an object");
 
       if (!_.isUndefined(body) && !_.isPlainObject(body))
-        throw new Error('Body must be an object');
+        throw new Error("Body must be an object");
 
       this.channel.call({
-        method: 'request',
+        method: "request",
         params: params,
-        success (response) { resolve(response) },
-        error (err) { reject(err) }
+        success(response) {
+          resolve(response);
+        },
+        error(err) {
+          reject(err);
+        },
       });
     });
   }
 
-  saveData (data) {
+  saveData(data) {
     return new Promise((resolve, reject) => {
-      if (!_.isPlainObject(data)) throw new Error('Data must be an object');
+      if (!_.isPlainObject(data)) throw new Error("Data must be an object");
 
       this.channel.call({
-        method: 'saveData',
+        method: "saveData",
         params: data,
-        success () { resolve() },
-        error (err) { reject(err) }
+        success() {
+          resolve();
+        },
+        error(err) {
+          reject(err);
+        },
       });
     });
   }
 
-  addTransaction (attrs) {
+  addTransaction(attrs) {
     return new Promise((resolve, reject) => {
       if (!_.isUndefined(attrs) && !_.isPlainObject(attrs))
-        throw new Error('Attrs must be an object');
+        throw new Error("Attrs must be an object");
 
       this.channel.call({
-        method: 'addTransaction',
+        method: "addTransaction",
         params: attrs,
-        success (transaction) { resolve(transaction) },
-        error (err) { reject(err) }
+        success(transaction) {
+          resolve(transaction);
+        },
+        error(err) {
+          reject(err);
+        },
       });
     });
   }
 
-  editTransaction (id) {
+  editTransaction(id) {
     return new Promise((resolve, reject) => {
-      if (!id || !_.isString(id)) throw new Error('Invalid id');
+      if (!id || !_.isString(id)) throw new Error("Invalid id");
 
       this.channel.call({
-        method: 'editTransaction',
+        method: "editTransaction",
         params: id,
-        success (transaction) { resolve(transaction) },
-        error (err) { reject(err) }
+        success(transaction) {
+          resolve(transaction);
+        },
+        error(err) {
+          reject(err);
+        },
       });
     });
   }
 
-  addInstitution (attrs) {
+  addInstitution(attrs) {
     return new Promise((resolve, reject) => {
       if (!_.isUndefined(attrs) && !_.isPlainObject(attrs))
-        throw new Error('Attrs must be an object');
+        throw new Error("Attrs must be an object");
 
       this.channel.call({
-        method: 'addInstitution',
+        method: "addInstitution",
         params: attrs,
-        success (institution) { resolve(institution) },
-        error (err) { reject(err) }
+        success(institution) {
+          resolve(institution);
+        },
+        error(err) {
+          reject(err);
+        },
       });
     });
   }
 
-  downloadDocument (id) {
+  downloadDocument(id) {
     return new Promise((resolve, reject) => {
-      if (!id || !_.isString(id)) throw new Error('Invalid id');
+      if (!id || !_.isString(id)) throw new Error("Invalid id");
 
       this.channel.call({
-        method: 'downloadDocument',
+        method: "downloadDocument",
         params: id,
-        success () { resolve() },
-        error (err) { reject(err) }
+        success() {
+          resolve();
+        },
+        error(err) {
+          reject(err);
+        },
       });
     });
   }
@@ -138,14 +171,18 @@ class Addon extends EventEmitter {
   upgradePremium() {
     return new Promise((resolve, reject) => {
       this.channel.call({
-        method: 'upgradePremium',
-        success () { resolve() },
-        error (err) { reject(err) }
+        method: "upgradePremium",
+        success() {
+          resolve();
+        },
+        error(err) {
+          reject(err);
+        },
       });
     });
   }
 
-  destroy () {
+  destroy() {
     this.channel.destroy();
   }
 }
